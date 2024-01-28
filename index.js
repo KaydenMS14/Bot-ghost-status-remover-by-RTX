f07c79c7d1c1d332ac8526c8b66e212451d73f63
KaydenMS14 <128863642+KaydenMS14@users.noreply.github.com>
2024-01-27 23:59:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Your Links","Listening to the developer","Playing Generate Links"];
 
 
 let currentIndex = 0;
