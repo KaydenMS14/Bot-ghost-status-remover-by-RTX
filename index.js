ae399bdbd36c8b7fc21b44cd8f6f4725315eb1f5
KaydenMS14 <128863642+KaydenMS14@users.noreply.github.com>
2024-01-31 22:08:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Powered By Interstellar! .gg/darkturns",];
+const statusMessages = ["Powered By Interstellar .gg/darkturns",];
 
 
 let currentIndex = 0;
